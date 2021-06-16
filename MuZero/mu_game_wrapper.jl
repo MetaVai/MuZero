22c20485187d1dcda79edce492d0965e891cfeb1
Michał Łukomski <michal.lukomski21@gmail.com>
2021-06-16 15:27:50 +0200

    cachedOracle and proper copy

--- MuZero/mu_game_wrapper.jl
+++ MuZero/mu_game_wrapper.jl
@@ -10,13 +10,13 @@ end
     nn = nothing
 end
 
-#batchified
+#batchify it
 function (representation_h::Representation)(state)
     hidden_state = state 
     return hidden_state
 end
 
-#batchified
+#batchify it 
 function (dynamics_g::Dynamics)(hidden_state, action)
     game_g = GI.init(dynamics_g.gspec, hidden_state)
     GI.play!(game_g, action)
@@ -25,55 +25,60 @@ function (dynamics_g::Dynamics)(hidden_state, action)
     return (reward, next_hidden_state)
 end
 
+# maybe add max_size (?), but it is cleared every think() run
+mutable struct CachedOracle{O, K, V}
+    oracle :: O
+    lookuptable :: Dict{K,V} # Dict{Tuple{typeof(hidden_state), Int},Tuple{Float64, typeof(hidden_state)}}()
+end
+
+function (oracle::CachedOracle)(args...)
+    if haskey(oracle.lookuptable, args)
+        answer = oracle.lookuptable[args]
+    else
+        answer = oracle.oracle(args...)
+        oracle.lookuptable[args] = answer
+    end
+    return answer
+end
 
 #naming: dynamics_oracle, representation_oracle 
-# or dynamics_g, representation_h
+# or dynamics_g, representation_h, or just dynamics, representation
 
-mutable struct MuGameEnvWrapper{G, S, A} <: GI.AbstractGameEnv
-    game :: G
-    dynamics_oracle :: Dynamics # g(s,a) -> r,s 
-    representation_oracle :: Representation # h(0) -> s
-    curstate :: S
-    rootstate :: S # utilize it better (play! should set it to false, and clone to reset)
-    white_playing :: Bool
-    lookuptable :: Dict{Tuple{S,A},Tuple{Float64,S}} # (state,action) => (reward, state) #maybe structs? or named tuples
+mutable struct MuGameEnvWrapper{State, D, R} <: GI.AbstractGameEnv
+    game :: GI.AbstractGameEnv
+    dynamics_oracle :: D # g(s,a) -> r,s 
+    representation_oracle :: R # h(0) -> s
+    curstate :: State
+    isrootstate :: Bool
+    white_playing :: Bool # MuWrapper changes player each round
     lastreward :: Float64
 end
 
-# GI.game_terminated(game::MuGameEnvWrapper) = false 
-GI.game_terminated(game::MuGameEnvWrapper) = GI.game_terminated(GI.init(GI.spec(game.game),game.curstate))
+GI.game_terminated(game::MuGameEnvWrapper) = false 
+# GI.game_terminated(game::MuGameEnvWrapper) = GI.game_terminated(GI.init(GI.spec(game.game),game.curstate))
 
 GI.current_state(game::MuGameEnvWrapper) = game.curstate
 
-GI.available_actions(game::MuGameEnvWrapper) = game.curstate == game.rootstate ? GI.available_actions(game.game) : GI.actions(GI.spec(game.game))
+GI.available_actions(game::MuGameEnvWrapper) = game.isrootstate ? GI.available_actions(game.game) : GI.actions(GI.spec(game.game))
 
 GI.white_playing(game::MuGameEnvWrapper) = game.white_playing
 
 function GI.play!(game::MuGameEnvWrapper, action)
-    if haskey(game.lookuptable, (game.curstate, action))
-        (game.lastreward, game.curstate) = game.lookuptable[(game.curstate,action)]
-    else
-        (R, S) = game.dynamics_oracle(game.curstate, action)
-        game.lookuptable[(game.curstate, action)] = (R, S)
-        (game.lastreward, game.curstate) = (R, S)
-        @debug "New entry in lookuptable" game.lookuptable
-    end
+    # (R, S) = game.dynamics_oracle(game.curstate, action)
+    (game.lastreward, game.curstate) = game.dynamics_oracle(game.curstate, action)
     game.white_playing = !game.white_playing
 end
 
 GI.white_reward(game::MuGameEnvWrapper) = game.lastreward #vulnerable when call white_reward() before play!()
 
-"""
-    WARNING
-    This function do not clone wrapper, 
-    It reset curstate to rootstate and return the same wrapper
-
-    That way there is one lookuptable shared between MCTS simulations
-    (copying large lookuptable would be costly)
-"""
 function GI.clone(game::MuGameEnvWrapper) # make lookuptable shallowcopy, or pass it other way
-    game.curstate = game.rootstate
-    return game
+    MuGameEnvWrapper(GI.clone(game.game),
+    game.dynamics_oracle,
+    game.representation_oracle,
+    game.curstate,
+    game.isrootstate,
+    game.white_playing,
+    game.lastreward)
 end
 
 
